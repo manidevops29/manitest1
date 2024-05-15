6421acafc5a5a3f31d2a302a827f0384f23ce02f
devops <hclfastleap@gmail.com>
2024-05-15 19:06:34 +0000

    udpated app

--- application.py
+++ application.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcom():
-    return "<html><body><h1>Chandramani's App!</h1></body></html>"
+    return "<html><body><h1>Chandramani's App! Updated file</h1></body></html>"
