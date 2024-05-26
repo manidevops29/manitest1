b712b3e4e5c4a12254eb31e49490e3ca1a5f34ba
mani <hclfastleap@gmail.com>
2024-05-27 03:12:18 +0530

    New Az admin resume

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
+
